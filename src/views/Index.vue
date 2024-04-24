6af3c8982e4e2e526231f9b19d9c829204b897da
kibalias <nikojancasillan@gmail.com>
2024-04-25 00:55:34 +0800

    Toggle for light and dark theme

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,12 +1,20 @@
 <script setup>
-
 import LandingPage from "@/views/LandingPage.vue";
 import ContentSide from "@/views/ContentSide.vue";
+import {ref} from "vue";
+
+const isLightTheme = ref(true)
+
+function toggleTheme() {
+  isLightTheme.value = !isLightTheme.value
+  console.log(isLightTheme.value)
+}
 </script>
 
 <template>
-  <div>
-    <iconify-icon icon="ph:sun-bold" width="1.7rem" class="sun-icon"></iconify-icon>
+  <div class="main-container" :class="{'dark-theme' : !isLightTheme}" style="color: var(--text-color)">
+    <iconify-icon v-if="!isLightTheme" icon="ri:moon-line" width="1.7rem" class="moon-icon" style="color: #d4d4d4" @click="toggleTheme"></iconify-icon>
+    <iconify-icon v-else icon="ph:sun-bold" width="1.7rem" class="sun-icon" @click="toggleTheme"></iconify-icon>
     <div class="d-md-flex">
       <div class="index-left">
         <landing-page></landing-page>

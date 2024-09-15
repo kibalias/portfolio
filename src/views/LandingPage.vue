6d0ca4aba7badabc50c2a3cf2eba7c91aac3e534
kibalias <nikojancasillan@gmail.com>
2024-09-16 03:47:06 +0800

    added links for socials

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -16,9 +16,15 @@
       Currently doing frontend and backend development but mainly focusing on frontend development. Based in Manila, Philippines.
     </div>
     <div class="socials">
-      <iconify-icon icon="charm:github" width="1.1em" height="1.1em"></iconify-icon>
-      <iconify-icon icon="ri:linkedin-fill" width="1.2em" height="1.2em"></iconify-icon>
-      <iconify-icon icon="ic:baseline-alternate-email" width="1.1em" height="1.1em"></iconify-icon>
+      <a href="https://github.com/kibalias" target="_blank">
+        <iconify-icon icon="charm:github" width="1.1em" height="1.1em"></iconify-icon>
+      </a>
+      <a href="https://www.linkedin.com/in/jan-nicole-casillan/" target="_blank">
+        <iconify-icon icon="ri:linkedin-fill" width="1.2em" height="1.2em"></iconify-icon>
+      </a>
+      <a href="mailto:nikojancasillan@gmail.com" target="_blank">
+        <iconify-icon icon="ic:baseline-alternate-email" width="1.1em" height="1.1em"></iconify-icon>
+      </a>
     </div>
   </div>
 </div>
